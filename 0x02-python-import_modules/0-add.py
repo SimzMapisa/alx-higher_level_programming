5fd8d842135accab84db4bb25154c3052c22db5e
SimzMapisa <simbarashe795@gmail.com>
2023-11-02 12:08:54 +0200

    change the import and fix the if condition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-from add-0 import add
+if __name__ == "__main__":
+    from add-0 import add
+    a = 1
+    b = 2
 
-if __name__ = "__main__":
-    def handleSum(a, b):
-        a = 1
-        b = 2
+    result = add(a, b)
 
-        result = add(a, b)
-
-        print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, result))
