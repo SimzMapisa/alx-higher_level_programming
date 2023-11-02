37e2159bc3b1b7d48935f58baf8e4306722a6609
SimzMapisa <simbarashe795@gmail.com>
2023-11-02 16:24:21 +0200

    fix code output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 
+from sys import argv
+
+
+def main():
+    length = len(argv) - 1
+    if length == 0:
+        print("{0:d} arguments.".format(length))
+    elif length == 1:
+        print("{0:d} argument:".format(length))
+    else:
+        print("{0:d} arguments:".format(length))
+    for i in range(1, length + 1):
+        print("{0}: {1}".format(i, argv[i]))
+
+
 if __name__ == "__main__":
-    import sys
-
-    argv = sys.argv
-    def printArgs (args):
-        argCount = 0
-        argsLen = len(args) - 1
-
-        if argsLen == 0:
-            print("{} arguments.".format(argsLen))
-        elif argsLen == 1:
-            print("{} argument:".format(argsLen))
-            print("{}: {}".format(argsLen, argv[1]))
-        else:
-            print("{} arguments:".format(argsLen))
-            for arg in argv:
-                if arg[0][0] == '.':
-                    continue
-                argCount += 1
-                print("{}: {}".format(argCount, arg))
-
-    printArgs(argv)
-
+    main()
