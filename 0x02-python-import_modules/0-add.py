ed7ae7f39a238a286fb70e7f08f50220add24055
SimzMapisa <simbarashe795@gmail.com>
2023-11-02 12:05:44 +0200

    fix code output and requirements

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,11 @@
 
 from add-0 import add
 
-def handleSum(a, b):
-    a = 1
-    b = 2
+if __name__ = "__main__":
+    def handleSum(a, b):
+        a = 1
+        b = 2
 
-    result = add(a, b)
+        result = add(a, b)
 
-    print("{} + {} = {}".format(a, b, result))
+        print("{} + {} = {}".format(a, b, result))
