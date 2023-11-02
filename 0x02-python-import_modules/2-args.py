a206cd1bb830bd58287d302f8b57cab5f8e0e92f
SimzMapisa <simbarashe795@gmail.com>
2023-11-02 13:07:51 +0200

    fix requirement issue

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
 
+#if __name__ == "__main__":
+#   import sys
+
+#    argv = sys.argv
+#    def printArgs (args):
+#        argCount = 0
+#        argsLen = len(args) - 1
+
+#        if argsLen == 0:
+#            print("{} arguments.".format(argsLen))
+#        elif argsLen == 1:
+#            print("{} argument:".format(argsLen))
+#            print("{}: {}".format(argsLen, argv[1]))
+#        else:
+#            print("{} arguments:".format(argsLen))
+#            for arg in argv:
+#                if arg[0][0] == '.':
+#                    continue
+#                argCount += 1
+#                print("{}: {}".format(argCount, arg))
+
+#    printArgs(argv)
+
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    
-    argv = sys.argv
-    def printArgs (args):
-        argCount = 0
-        argsLen = len(args) - 1
-
-        if argsLen == 0:
-            print("{} arguments.".format(argsLen))
-        elif argsLen == 1:
-            print("{} argument:".format(argsLen))
-            print("{}: {}".format(argsLen, argv[1]))
-        else:
-            print("{} arguments:".format(argsLen))
-            for arg in argv:
-                if arg[0][0] == '.':
-                    continue
-                argCount += 1
-                print("{}: {}".format(argCount, arg))
-
-    printArgs(argv)
+    total = len(sys.argv) - 1
+    if total == 0:
+        print("0 arguments.")
+    elif total == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(total))
+    for p in range(total):
+        print("{}: {}".format(p + 1, sys.argv[p + 1]))
