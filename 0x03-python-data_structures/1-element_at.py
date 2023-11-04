2d51d54386a6f8b89994ef8370ecd2a2b88e77f3
SimzMapisa <simbarashe795@gmail.com>
2023-11-04 08:07:16 +0200

    fix output error of None

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
+    if len(my_list) == 0:
+        return None
     if idx < 0 or idx > len(my_list):
-        return none
+        return None
     return my_list[idx]
