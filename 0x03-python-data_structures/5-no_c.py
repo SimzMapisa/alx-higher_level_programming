20ef52a9e00212181a0ca3363a5250cb66d04a70
SimzMapisa <simbarashe795@gmail.com>
2023-11-04 16:04:38 +0200

    fix my code logic

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            modify = my_string.replace('c', "").replace('C', "")
-
-    return modify
+    str_list = []
+    for char in my_string:
+        if char not in "cC":
+            str_list.append(char)
+    return "".join(str_list)
