4c1617bffcaf118fbf59870c08350bd58f30c689
SimzMapisa <simbarashe795@gmail.com>
2023-11-06 12:26:18 +0200

    fix requirement issue

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return(0, None)
-    length =  len(sentence)
-    first_char = sentence[0]
-    res = (length, first_char)
-    return(res)
-
+        return (0, None)
+    result = (len(sentence), sentence[0])
+    return result
