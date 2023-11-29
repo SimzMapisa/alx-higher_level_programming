cb5d5a058018a01ee781a358e3fcb00108df4a05
SimzMapisa <simbarashe795@gmail.com>
2023-11-29 05:47:26 +0000

    fix pycodestyle errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,25 +3,24 @@
 
 
 def add_integer(a, b=98):
-	"""Adds two integers or float values.
+    """Adds two integers or float values.
 
-	Args:
-	    a: The first parameter.
-	    b: The second parameter.
+    Args:
+        a: The first parameter.
+        b: The second parameter.
 
-	Returns:
-	    An integer addition of a and b.
+    Returns:
+        An integer addition of a and b.
 
-	Float values are first typecasted to int before adding them.
+    Float values are first typecasted to int before adding them.
 
-	Raises:
-	    TypeError: If either a or b is not an integer or a float type.
-	"""
+    Raises:
+        TypeError: If either a or b is not an integer or a float type.
+    """
 
-	if not isinstance(a, (int, float)):
-		raise TypeError("a must be an integer")
-	elif not isinstance(b, (int, float)):
-		raise TypeError("b must be an integer")
-	else:
-		return (int(a) + int(b))
-
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return (int(a) + int(b))
