098aff225eba73247f390f4282522a3bfbcec67f
SimzMapisa <simbarashe795@gmail.com>
2024-02-22 13:54:04 +0000

    fix code logic

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (sentence in sentences) { console.log(sentence) };
+for (let sentence of sentences) { console.log(sentence) };
