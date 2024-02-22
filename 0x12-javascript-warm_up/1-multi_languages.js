5fd3f0127512e3af0f5227aa8829f0f8c6b0f0d6
SimzMapisa <simbarashe795@gmail.com>
2024-02-22 13:57:05 +0000

    fix requirements issue

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let sentence of sentences) { console.log(sentence) };
+for (const sentence of sentences) { console.log(sentence) };
